fc5c997d657c89387fbe2ccb742904764bfc0d16
karvit311 <gjtlff888@gmail.com>
2018-08-11 16:46:37 +0300

    adding files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/vendor/autoload.php');
+require_once(ROOT.'/application/core/route.php');
+require_once(ROOT.'/database.php');
+ 
+// подключаем конфигурацию URL
+session_start([
+    'cookie_lifetime' => 86400,
+]);
+$routes=ROOT.'/application/routes.php';
+$router = new Application\core\App();
+$router->run();
