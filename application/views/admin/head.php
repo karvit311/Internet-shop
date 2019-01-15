97073a4360fef8b405c10d80e665ab319404b006
liubava <gjtlff888@gmail.com>
2019-01-15 16:46:41 +0300

    head script

--- application/views/admin/head.php
+++ application/views/admin/head.php
@@ -1,84 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<!--    <title></title>-->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote.min.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-bs4.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-bs4.min.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-lite.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-lite.min.css">
-
-    <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.css">
-    <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link rel="stylesheet" type="text/css" href="/application/js/bootstrap-wysihtml5-master/lib/css/prettify.css"/>
-    <link rel="stylesheet" type="text/css" href="/application/js/bootstrap-wysihtml5-master/src/bootstrap-wysihtml5.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css" />
-    <link rel="stylesheet" href="/application/css/site2.css">
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="/application/css/css.css">
-
-    <link rel="stylesheet" href="/application/css/admin.css">
-    <link rel="stylesheet" href="/application/css/newsAdmin.css">
-    <link rel="stylesheet" href="/application/css/footer.css">
-
-<!--    <script type="text/javascript">-->
-<!--        var jQuery_1_12_2 = $.noConflict(true);-->
-<!--    </script>-->
-
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>-->
-<!--    <script src="https://code.jquery.com/jquery-1.12.2.min.js" integrity="sha256-lZFHibXzMHo3GGeehn1hudTAP3Sc0uKXBXAzHX1sjtk=" crossorigin="anonymous"></script>-->
-    <script
-            src="https://code.jquery.com/jquery-1.9.0.js"
-            integrity="sha256-TXsBwvYEO87oOjPQ9ifcb7wn3IrrW91dhj6EMEtRLvM="
-            crossorigin="anonymous"></script>
-<!--    <script src="//code.jquery.com/jquery-1.10.2.js"></script>-->
-<!--    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>-->
-
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>-->
-    <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
-    <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-
-    <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-    <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
-<!--    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css"/>-->
-<!--    <script src=" https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>-->
-<!--    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>-->
-
-    <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
-
-    <script type="text/javascript"  src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-    <script src="/application/js/jcarousellite_1.1.js"></script>
-    <script src="/application/js/popper/popper.js"></script>
-    <script src="/application/js/popper/popper.min.js"></script>
-    <script src="/application/js/popper/tooltip.js"></script>
-    <script src="/application/js/popper/tooltip.min.js"></script>
-    <script src="/application/js/dist/summernote.js"></script>
-    <script src="/application/js/dist/summernote.min.js"></script>
-    <script src="/application/js/dist/summernote-bs4.js"></script>
-    <script src="/application/js/dist/summernote-bs4.min.js"></script>
-    <script src="/application/js/dist/summernote-lite.js"></script>
-    <script src="/application/js/dist/summernote-lite.min.js"></script>
-<!--    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>-->
-    <script src="/application/js/JQuery-Session-Plugin-master/jquery.session.js"></script>
-
-
-
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote.min.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-bs4.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-bs4.min.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-lite.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-lite.min.css">
+        <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.css">
+        <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="/application/js/bootstrap-wysihtml5-master/lib/css/prettify.css"/>
+        <link rel="stylesheet" type="text/css" href="/application/js/bootstrap-wysihtml5-master/src/bootstrap-wysihtml5.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css" />
+        <link rel="stylesheet" href="/application/css/site2.css">
+        <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="/application/css/css.css">
+        <link rel="stylesheet" href="/application/css/admin.css">
+        <link rel="stylesheet" href="/application/css/newsAdmin.css">
+        <link rel="stylesheet" href="/application/css/footer.css">
+        <script src="https://code.jquery.com/jquery-1.9.0.js" integrity="sha256-TXsBwvYEO87oOjPQ9ifcb7wn3IrrW91dhj6EMEtRLvM=" crossorigin="anonymous"></script>
+        <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+        <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript"  src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+        <script src="/application/js/jcarousellite_1.1.js"></script>
+        <script src="/application/js/popper/popper.js"></script>
+        <script src="/application/js/popper/popper.min.js"></script>
+        <script src="/application/js/popper/tooltip.js"></script>
+        <script src="/application/js/popper/tooltip.min.js"></script>
+        <script src="/application/js/dist/summernote.js"></script>
+        <script src="/application/js/dist/summernote.min.js"></script>
+        <script src="/application/js/dist/summernote-bs4.js"></script>
+        <script src="/application/js/dist/summernote-bs4.min.js"></script>
+        <script src="/application/js/dist/summernote-lite.js"></script>
+        <script src="/application/js/dist/summernote-lite.min.js"></script>
+        <script src="/application/js/JQuery-Session-Plugin-master/jquery.session.js"></script>
+    </head>
 <style>
     .dropdown-menu a{
         background:white;
@@ -140,7 +109,6 @@
                         <label for="adminAboutContent">Content</label>
                         <textarea class="form-control" id="adminAboutContent" rows="100">
                         </textarea>
-
                     </div>
                     <?php }?>
                 </form>
@@ -173,22 +141,15 @@
         });
         $('#about_company_admin').click(function(){
            var title =  $('#adminAboutTitle').val();
-           // alert(title);
-           //  $('.big_description').summernote('code',description);
            var content = $.trim($('#adminAboutContent').val());
             var textareaValue = $('#adminAboutContent').summernote('code');
-            // alert(textareaValue);
-            // var textareaValue = $("#summernote").code();
-            // alert()
            $.ajax({
                type:"post",
                url:"/admin/UpdateAboutCompany",
                data:"title="+title+"&content="+textareaValue,
                success:function(res){
-                   alert(res);
                },
                error:function(){
-
                }
            })
         });
--- application/views/main/head.php
+++ application/views/main/head.php
@@ -1,93 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<!--    <title></title>-->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.css">
-    <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.css">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<!--    <link rel="stylesheet" href="/application/js/star-rating-svg-master/demo/css/demo.css">-->
-    <link rel="stylesheet" href="/application/js/star-rating-svg-master/src/css/star-rating-svg.css">
-    <link rel="stylesheet" href="/application/js/rater/styles/styles.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote.min.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-bs4.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-bs4.min.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-lite.css">
-    <link rel="stylesheet" href="/application/js/dist/summernote-lite.min.css">
-    <link rel="stylesheet" href="/application/css/site2.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css" />
-
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="/application/css/css.css">
-    <link rel="stylesheet" href="/application/css/footer.css">
-
-
-<!--    <script type="text/javascript">-->
-<!--        var jQuery_1_12_2 = $.noConflict(true);-->
-<!--    </script>-->
-
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>-->
-<!--    <script src="https://code.jquery.com/jquery-1.12.2.min.js" integrity="sha256-lZFHibXzMHo3GGeehn1hudTAP3Sc0uKXBXAzHX1sjtk=" crossorigin="anonymous"></script>-->
-    <script src="https://code.jquery.com/jquery-1.9.0.js" integrity="sha256-TXsBwvYEO87oOjPQ9ifcb7wn3IrrW91dhj6EMEtRLvM=" crossorigin="anonymous"></script>
-<!--    <script src="/application/js/cart-main.js"></script>-->
-<!--    <script src="//code.jquery.com/jquery-1.10.2.js"></script>-->
-<!--    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>-->
-
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>-->
-    <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
-    <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-
-<!--            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-<!--            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
-<!--    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css"/>-->
-<!--    <script src=" https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>-->
-<!--    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>-->
-
-    <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
-
-    <script type="text/javascript"  src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-    <script src="/application/js/jcarousellite_1.1.js"></script>
-    <script src="/application/js/popper/popper.js"></script>
-    <script src="/application/js/popper/popper.min.js"></script>
-    <script src="/application/js/popper/tooltip.js"></script>
-    <script src="/application/js/popper/tooltip.min.js"></script>
-    <script src="/application/js/dist/summernote.js"></script>
-    <script src="/application/js/dist/summernote.min.js"></script>
-    <script src="/application/js/dist/summernote-bs4.js"></script>
-    <script src="/application/js/dist/summernote-bs4.min.js"></script>
-    <script src="/application/js/dist/summernote-lite.js"></script>
-    <script src="/application/js/dist/summernote-lite.min.js"></script>
-    <script src="/application/js/star-rating-svg-master/src/jquery.star-rating-svg.js"></script>
-<!--    <script src="/application/js/cart-main.js"></script>-->
-<!--    <script src="/application/js/star-rating-svg-master/Gruntfile.js"></script>-->
-<!--    <script src="/application/js/star-rating-svg-master/dist/jquery.star-rating-svg.js"></script>-->
-
-
-<!--    <script src="/application/js/rater/js/jquery.rating-2.0.min.js"></script>-->
-<!--    <script src="/application/js/rater/js/jquery-1.6.2.min.js"></script>-->
-
-
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.css">
+        <link rel="stylesheet" href="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/application/js/star-rating-svg-master/src/css/star-rating-svg.css">
+        <link rel="stylesheet" href="/application/js/rater/styles/styles.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote.min.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-bs4.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-bs4.min.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-lite.css">
+        <link rel="stylesheet" href="/application/js/dist/summernote-lite.min.css">
+        <link rel="stylesheet" href="/application/css/site2.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css" />
+        <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="/application/css/css.css">
+        <link rel="stylesheet" href="/application/css/footer.css">
+        <script src="https://code.jquery.com/jquery-1.9.0.js" integrity="sha256-TXsBwvYEO87oOjPQ9ifcb7wn3IrrW91dhj6EMEtRLvM=" crossorigin="anonymous"></script>
+        <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+        <script src="/application/js/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript"  src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+        <script src="/application/js/jcarousellite_1.1.js"></script>
+        <script src="/application/js/popper/popper.js"></script>
+        <script src="/application/js/popper/popper.min.js"></script>
+        <script src="/application/js/popper/tooltip.js"></script>
+        <script src="/application/js/popper/tooltip.min.js"></script>
+        <script src="/application/js/dist/summernote.js"></script>
+        <script src="/application/js/dist/summernote.min.js"></script>
+        <script src="/application/js/dist/summernote-bs4.js"></script>
+        <script src="/application/js/dist/summernote-bs4.min.js"></script>
+        <script src="/application/js/dist/summernote-lite.js"></script>
+        <script src="/application/js/dist/summernote-lite.min.js"></script>
+        <script src="/application/js/star-rating-svg-master/src/jquery.star-rating-svg.js"></script>
+    </head>
     <body>
-    <style>
-         /*.dropdown-menu a{*/
-            /*background:white;*/
-        /*}*/
-    </style>
         <?php
         $ip_address = file_get_contents('https://api.ipify.org');
         if($_SESSION['email'] != ''){
@@ -116,8 +76,6 @@
                 </li>
                 <li class="list-group-item"><a href="/main/about">About us</a></li>
                 <?php
-                print_r($_SESSION);
-                //session_start();
                 if (session_status() != PHP_SESSION_NONE) {
                     if ($_SESSION['loggedin'] == 1) { ?>
                         <li class="list-group-item logout-main-index"><a href="#"><?= $_SESSION['name'] ?> (Выйти)</a></li>
